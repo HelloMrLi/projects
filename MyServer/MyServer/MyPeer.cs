c93382e71776ddc0ae88e2eb93b6edc4179f5566
HelloMrLi <1802171399@qq.com>
2016-12-23 23:39:38 +0800

    jia

---
+++ MyServer/MyServer/MyPeer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+using MyServer.Message;
+
+namespace MyServer
+{
+   public class MyPeer:ClientPeer
+    {
+        Hashtable userTabel;
+        public MyPeer(InitRequest initRequest) : base(initRequest)
+        {
+            userTabel = new Hashtable();
+            userTabel.Add("123", "1234");
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+              //取得Client端传过来的要求加以处理
+            switch (operationRequest.OperationCode)
+            {
+                case (byte)OpCodeEnum.Login:
+                    string uname = (string)operationRequest.Parameters[(byte)OpKeyEnum.UserName];
+                    string pwd = (string)operationRequest.Parameters[(byte)OpKeyEnum.PassWord];
+                    
+                    if (userTabel.ContainsKey(uname) && userTabel[uname].Equals(pwd))
+                    {
+                        SendOperationResponse(new OperationResponse((byte)OpCodeEnum.LoginSuccess, null), new SendParameters());
+                    }
+                    else
+                    {
+                        SendOperationResponse(new OperationResponse((byte)OpCodeEnum.LoginFailed, null), new SendParameters());
+                    }
+                    break;
+            }
+        }
+    }
+}
