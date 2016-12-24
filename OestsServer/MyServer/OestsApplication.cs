bb25273e3be8afb9789c69ed65b8db4d8475c17e
HelloMrLi <1802171399@qq.com>
2016-12-24 17:01:07 +0800

    修改了服务器名称

---
+++ OestsServer/MyServer/OestsApplication.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+
+namespace OestsServer
+{
+    public class OestsApplication:ApplicationBase
+    {
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new ChatPeer(initRequest);
+        }
+
+        protected override void TearDown()
+        {
+            
+        }
+
+        protected override void Setup()
+        {
+            
+        }
+    }
+}
